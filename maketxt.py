45fa44008daa083a47a54d1cb5f5f0a46157b6b8
Eduard Zhukov <35345123+ze-dev@users.noreply.github.com>
2018-12-22 18:46:02 +0300

    Add files via upload

---
+++ maketxt.py
@@ -0,0 +1,19 @@
+def maketxt(filename):
+    '''Создает текстовый файл в директории запуска скрипта.
+    В консоли вызывать maketxt.py НужноеИмя (имя без пробелов)'''
+    import time, os
+    with open('%s.txt' % filename, 'w') as ff:
+        dirPath=os.getcwd()
+        fullPath = '%s\%s' % (dirPath, filename)
+        print('\nДиректория назначения вызова > %s ' % dirPath)
+        print('\nФайл  %s.txt  создан...' % filename)
+        time.sleep(2.2)
+
+if __name__ == '__main__':
+    import sys
+    if len (sys.argv) > 1:
+        parameter = sys.argv[1]
+    else:
+        parameter = input('Введите имя файла >  ')
+    maketxt(parameter)
+    
