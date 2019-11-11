58eb016faa64a1080ce67a349d9b150680350f74
Eduard Zhukov <35345123+ze-dev@users.noreply.github.com>
2019-11-11 10:54:55 +0300

    Update getpath.py

--- getpath.py
+++ getpath.py
@@ -4,7 +4,7 @@ def getpath():
     a = os.getcwd() + '\\'
     pyperclip.copy(a)
     pyperclip.paste()
-    input('В буфер скопирован адрес >  \n%s ' % a)
+    input('В буфер скопирован путь >  \n%s ' % a)
 
 if __name__ == '__main__':
     getpath()
