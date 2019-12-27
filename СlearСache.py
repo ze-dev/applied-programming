79cc3f60e21da321e747ab0523fa553958619a69
Eduard Zhukov <35345123+ze-dev@users.noreply.github.com>
2019-12-27 11:58:39 +0300

    Update and rename СleanСache.py to СlearСache.py

--- СlearСache.py
+++ СlearСache.py
@@ -1,6 +1,6 @@
 import pyperclip,time
 t = input('Сформируем адрес доступа к кешу.\nТерминал?\n')
-c = input('Компьютер пользователя?\n')
+c = input('Пользователь Windows?\n')
 an='\\\\{}\\c$\\Users\\{}\\AppData\\'.format(t,c)
 pyperclip.copy(an)
 print('Адрес скопирован в буфер: \n'+an)
