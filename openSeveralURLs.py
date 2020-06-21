586ca64fe658a9e18e2577be5c6ed971dad80e31
Eduard Zhukov <35345123+ze-dev@users.noreply.github.com>
2020-06-21 18:28:23 +0300

    Update openSeveralURLs.py

--- openSeveralURLs.py
+++ openSeveralURLs.py
@@ -1,6 +1,11 @@
 '''Использовал для открытия сразу 20 разных url-ссылок,
 когда размещал объявления в разных группах VK и авито.
-Читает все из одного текстового файла в той же папке.'''
+Читает все из одного текстового файла в той же папке.
+Исходный может быть вида, обработает линии только с line.startswith('http'):
+выложил 21.06.20.
+https://vk.com/bryansk_pogoda             в предложенных
+
+'''
 
 import webbrowser, time
 from tkinter import Tk
@@ -15,9 +20,15 @@ with open(filename,'r') as stream:
 url_list = []
 for line in lines:
     if line.startswith('http'):
-        url_list.append(line.split(' ')[0][:-1])
+        arr = line.split()
+        if len(arr) > 0:
+            use = arr[0]
+        else:
+            use = arr[0][:-1]
+        url_list.append(use)
 
 for url in url_list:
+    #print(url)
     webbrowser.open_new_tab(url)
     time.sleep(3)
 
