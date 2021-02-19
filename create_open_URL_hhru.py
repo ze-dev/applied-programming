3bdb9a52018b8dc89eec67bd4d4bd202edf46890
Eduard Zhukov <35345123+ze-dev@users.noreply.github.com>
2021-02-19 19:28:10 +0300

    Update create_open_URL_hhru.py

--- create_open_URL_hhru.py
+++ create_open_URL_hhru.py
@@ -5,13 +5,13 @@
 
 import pyperclip, webbrowser
 
-vac_num = str(input("Введите номер (id) вакансии: \n "))
-vac_num = "37415007"  # test
+vac_num = str(input("Введите номер (id) вакансии: \n"))
+##vac_num = "37415007"  # test
 
 # create needed page
 page_url = r"https://hh.ru/vacancy/{}".format(vac_num)
 pyperclip.copy(page_url)
-print('Адрес вакансии скопирован в буфер: \n'+ page_url)
+print("Адрес вакансии скопирован в буфер: \n" + page_url)
 
 # open our created page in needed broweser
 vivaldi_path = r"C:\Program Files\Vivaldi\Application\vivaldi.exe" 
